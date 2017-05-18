1b8392e35d6f08bd8fe4aa62ec39fe3ce1bc2bd4
Denis Stepulenok <stden@Deniss-MacBook-Pro.local>
2017-05-18 20:28:00 +0300

    Tests runs with node.js & code coverage

--- change.js
+++ change.js
@@ -1,3 +1,10 @@
+/* The code block below ONLY Applies to Node.js - This Demonstrates
+ re-useability of JS code in both Back-end and Front-end! #isomorphic */
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = getChange;  // allows CommonJS/Node.js require()
+}
+
 var coins = [200, 100, 50, 20, 10, 5, 2, 1];
 function getChange(totalPayable, cashPaid) {
     var change = [];
--- test.js
+++ test.js
@@ -1,3 +1,14 @@
+/* The code block below ONLY Applies to Node.js - This Demonstrates
+ re-useability of JS code in both Back-end and Front-end! #isomorphic */
+/* istanbul ignore if */
+if (typeof module !== 'undefined' && module.exports) {
+    var QUnit = require('qunitjs'); // require QUnit node.js module
+    // alias the QUnit.test method so we don't have to change all our tests
+    var test = QUnit.test; // stores a copy of QUnit.test
+    require('qunit-tap')(QUnit, console.log); // use console.log for test output
+    var getChange = require('./change.js'); // load our getChange method
+}
+
 test('getChange(1,1) should equal [] - an empty array', function (assert) {
     var result = getChange(1, 1); //no change/coins just an empty array
     var expected = [];
@@ -21,3 +32,8 @@ test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', functio
     var expected = [200, 100, 50, 20, 10, 5, 2, 1];
     assert.deepEqual(result, expected);
 });
+
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) {
+    QUnit.load();
+} // run the tests
